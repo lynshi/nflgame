55552503e85ccf8b9c7ab9806a10b98c1915f761
Lyndon Shi <shilyndon@gmail.com>
2019-05-16 13:42:13 -0400

    extract all statistic names

--- nfldatabase/dbbuilder.py
+++ nfldatabase/dbbuilder.py
@@ -1,5 +1,6 @@
 from nfldatabase.database import NFLDatabase
 
+import datetime
 import nflgame
 
 
@@ -15,6 +16,10 @@ class NFLdbBuilder:
         self.db.create_players_table()
         self.db.create_teams_table()
         self.db.create_games_table()
+
+        stat_columns = self._find_stat_columns()
+        self.db.create_player_game_statistics_table(stat_columns)
+        self.db.create_team_game_statistics_table(stat_columns)
 
     def run(self):
         """
@@ -34,3 +39,30 @@ class NFLdbBuilder:
         :return: None
         """
         self.db.insert_teams(nflgame.teams)
+
+    def _find_stat_columns(self):
+        """
+        Find all statistic names for players in nflgame.
+
+        :return: list of set of statistic names
+        """
+
+        stat_columns = set()
+        phases = [('PRE', 4), ('REG', 17), ('POST', 4)]
+        seasons = [i for i in range(2009, datetime.datetime.now().year)]
+
+        for season in seasons:
+            for phase, num_weeks in phases:
+                for week in range(1, num_weeks + 1):
+                    try:
+                        games = nflgame.games(year=season, week=week,
+                                              kind=phase)
+                    except TypeError:
+                        continue
+
+                    players = nflgame.combine_game_stats(games)
+                    for player in players:
+                        for stat in player._stats:
+                            stat_columns.add(stat)
+
+        return stat_columns
